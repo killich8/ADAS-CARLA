46366d1a67c40986da09040bd92317fac6bd04eb
Jonas Killich <youness.killich8@gmail.com>
2025-10-17 01:10:33 +0200

    test: add basic unit tests

---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+def test_project_import():
+    """Test that the project can be imported."""
+    import src
+    assert src is not None
+
+def test_environment():
+    """Test environment setup."""
+    assert True
